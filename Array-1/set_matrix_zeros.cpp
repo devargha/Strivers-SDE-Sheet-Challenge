59489fede7fb685a0bad2c7de37c483bbe5b72e0
devargha <136103266+devargha@users.noreply.github.com>
2023-06-09 21:41:44 +0530

    Update set_matrix_zeros.cpp

--- Array-1/set_matrix_zeros.cpp
+++ Array-1/set_matrix_zeros.cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<int> r,c;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j]==0){
+                r.push_back(i);
+                c.push_back(j);}// now we have the indices where the value in the matrix is zero
+            }
+        }
+        //now we have to make the column values zero for every row 
+        for(int i=0;i<r.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                matrix[r[i]][j] = 0;
+            }
+        }
+        // now we make the row values zero for every column
+        for(int i=0;i<c.size();i++){
+            for(int j=0;j<matrix.size();j++){
+                matrix[j][c[i]] = 0;
+            }
+        }
+        
+    }
+};
